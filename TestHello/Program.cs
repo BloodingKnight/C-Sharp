61d930bd2378b4c6cbfa188ca871997e2af4d0a6
BloodingKnight <unlimited@bupt.edu.cn>
2015-04-16 19:15:47 +0800

    hiho1 初稿，无任何优化

---
+++ TestHello/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestHello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine(); //Wait for the user to press the Enter key
+        }
+    }
+}
--- hiho1/Program.cs
+++ hiho1/Program.cs
@@ -14,10 +14,45 @@ namespace hiho1
         static void Main(string[] args)
         {
             int length = int.Parse(Console.ReadLine());
-            while (length > 0)
+            int[] results = new int[length];
+            while (length-- > 0)
             {
+                int len = 0;
                 string line = Console.ReadLine();
+                char[] array = line.ToCharArray();
+                for (int i = 0; i < array.Length; i++)
+                {
+                    for (int j = 0; j < array.Length; j++)
+                    {
+
+                        len = isPalindrome(array, i, j) ? (j - i + 1 > len ? j - i + 1 : len) : len;
+                    }
+                }
+
+                results[length] = len;
             }
+
+            for (int i = results.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine(results[i]);
+            }
+        }
+
+        private static bool isPalindrome(char[] array, int i, int j)
+        {
+            while (i < j)
+            {
+                if (array[i] == array[j])
+                {
+                    ++i;
+                    --j;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
