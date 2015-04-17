4a79bb29990b39ba0f618813333fc17218c62e6c
BloodingKnight <unlimited@bupt.edu.cn>
2015-04-17 15:27:12 +0800

    1. 重新调整了代码的目录结构 2. 新建leetcode相关代码 3. 新建leetcode “Merge k Sorted Lists” 项目 4. hiho上，最长回文子序列方法重新优化，从中心点开始寻找

---
+++ hihoCode/hiho1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+///<summary>
+///最长回文字串
+///</summary>
+namespace hiho1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = int.Parse(Console.ReadLine());
+            int[] results = new int[length];
+            for (int i = 0; i < length; i++)
+            {
+                string line = Console.ReadLine();
+                int max = 0, tmp = 0;
+                for (int j = 0; j < line.Length; j++)
+                {
+                    tmp = getMaxPalindrome(line, j - 1, j + 1);
+                    max = max > tmp ? max : tmp;
+                    tmp = getMaxPalindrome(line, j, j + 1);
+                    max = max > tmp ? max : tmp;
+                }
+                results[i] = max;
+            }
+
+            for (int i = 0; i < length; i++)
+            {
+                Console.WriteLine(results[i]);
+            }
+
+        }
+
+        private static int getMaxPalindrome(string line, int i, int j)
+        {
+            while (i >= 0 && j < line.Length && line[i] == line[j])
+            {
+                --i;
+                ++j;
+            }
+
+            return j - i - 1;
+        }
+
+    }
+}
