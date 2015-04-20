c2fc18dcd992f92bab42cb6360276df971704771
BloodingKnight <unlimited@bupt.edu.cn>
2015-04-20 16:34:07 +0800

    Island Travel 使用Dijstra算法，已优化，只使用两个一维数组而不是一个二维数组

--- hihoCode/Islands Travel/Program.cs
+++ hihoCode/Islands Travel/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 ///<summory>
 ///Dijstra
@@ -13,6 +9,54 @@ namespace Islands_Travel
     {
         static void Main(string[] args)
         {
+            int size = int.Parse(Console.ReadLine());
+            int[] X = new int[size];
+            int[] Y = new int[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                string[] words = Console.ReadLine().Split(' ');
+                X[i] = int.Parse(words[0]);
+                Y[i] = int.Parse(words[1]);
+            }
+
+            bool[] visited = new bool[size];
+            int[] distance = new int[size];
+            int min = 0, last = 0;
+            for (int j = 1; j < size; j++)
+            {
+                distance[j] = caculate(X, Y, 0, j);
+                min = distance[min] < distance[j] ? min : j;
+            }
+            distance[0] = int.MaxValue;
+            for (int i = 1; i < size; i++)
+            {
+                visited[min] = true;
+                last = min;
+                min = 0;
+                for (int j = 1; j < size; j++)
+                {
+                    if (!visited[j])
+                    {
+                        int tmp = distance[last] + caculate(X, Y, last, j);
+                        distance[j] = distance[j] < tmp ? distance[j] : tmp;
+                        min = distance[min] < distance[j] ? min : j;
+                    }
+                }
+                if (min == size - 1)
+                {
+                    Console.WriteLine(min);
+                }
+            }
+
+        }
+
+        private static int caculate(int[] X, int[] Y, int i, int j)
+        {
+            int x = Math.Abs(X[i] - X[j]);
+            int y = Math.Abs(Y[i] - Y[j]);
+
+            return x < y ? x : y;
         }
     }
 }
